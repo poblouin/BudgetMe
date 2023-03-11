94128aad4fa1f0590f8e1ef7e9e2e699f1d0cc87
poblouin <poblouin@protonmail.com>
2023-03-11 15:48:46 -0500

    add eslint

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      eslint({ fix: true}),
+      vue()
+  ],
   resolve: {
       alias: {
           '~': '/src',
