071bfa2cc6b9eb33b2e06d209e018be3e63001df
poblouin <poblouin@protonmail.com>
2023-08-07 19:57:05 -0400

    tests

--- spec/lib/transactions/import_spec.rb
+++ spec/lib/transactions/import_spec.rb
@@ -10,7 +10,22 @@ RSpec.describe Transactions::Import do
       let(:csv_file) { tangerine_csv }
 
       it 'parses the report and saves the data to the database' do
-        import
+        expect { import }.to change(Transaction, :count).by(10)
+      end
+
+      it 'creates the transactions with the correct attributes' do
+        expect(Transaction.first.attributes.except(:id, :created_at, :updated_at).symbolize_keys).to match(
+          hash_including(
+            date: Date.new(2023, 8, 3),
+            amount: BigDecimal('-23.99'),
+            description: 'SOME FOOD',
+            transaction_category_id: TransactionCategory.find_by(name: 'Restaurant').id
+          )
+        )
+      end
+
+      it 'creates the associated transaction category' do
+        expect { import }.to change(TransactionCategory, :count).by(5)
       end
     end
 
@@ -19,7 +34,11 @@ RSpec.describe Transactions::Import do
       let(:csv_file) { cibc_csv }
 
       it 'parses the report and saves the data to the database' do
-        import
+        expect { import }.to change(Transaction, :count).by(10)
+      end
+
+      it 'creates the associated transaction category' do
+        expect { import }.to change(TransactionCategory, :count).by(1)
       end
     end
   end
