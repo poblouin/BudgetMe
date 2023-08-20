2981ef56f51bf94fecc6f229e1f5a79dbae6fb71
poblouin <poblouin@protonmail.com>
2023-08-19 20:10:46 -0400

    fetch transactions for date

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -1,15 +1,44 @@
 <script setup>
-import {computed, ref} from "vue";
+import { eachMonthOfInterval, endOfMonth, format, formatISO, getYear } from "date-fns";
+import { computed, ref } from "vue";
 import { useQuery } from '@vue/apollo-composable'
-import { VDataTable } from 'vuetify/labs/VDataTable'
-import { formatISO } from "date-fns";
+import { VDataTable } from "vuetify/labs/components";
 import { transactions } from "@graphql/queries.js";
 
-const transactionsQuery = useQuery(transactions, { from: formatISO(new Date(2023, 7, 1)) })
+// Date range
+const monthsList = () => {
+  const currentDate = new Date();
+
+  return eachMonthOfInterval({
+    start: new Date(getYear(currentDate) - 1, 0, 1),
+    end: currentDate,
+  }).reverse().map((date) => {
+    return {
+      title: format(date, 'yyyy MMMM'),
+      value: date,
+    }
+  })
+};
+const dateRange = monthsList();
+const selectedDate = ref(dateRange[0].value);
+
+// Transactions
+const transactionQueryVariables = computed(() => {
+  if (!selectedDate?.value) return;
+
+  const date = selectedDate.value
+  return {
+    from: formatISO(date, { representation: 'date' }),
+    to: formatISO(endOfMonth(date), { representation: 'date' })
+  }
+})
+const transactionsQuery = useQuery(transactions, transactionQueryVariables)
 const transactionsItems = computed(() => transactionsQuery.result?.value?.transactions ?? [])
+
+// Data table
 const itemsPerPage = -1 // all
 const search = ref('')
-const sortBy = [{key: 'date', order: 'desc'}]
+const sortBy = [{ key: 'date', order: 'desc' }]
 
 const headers = [
   { title: 'Date', key: 'date' },
@@ -21,6 +50,14 @@ const headers = [
 
 <template>
   <v-card width="100vw">
+    <v-card-title>
+      Date Range
+      <v-spacer />
+      <v-select
+        v-model="selectedDate"
+        :items="dateRange"
+      />
+    </v-card-title>
     <v-card-title>
       Transactions
       <v-spacer />
@@ -38,7 +75,7 @@ const headers = [
       :items="transactionsItems"
       :search="search"
       :sort-by="sortBy"
-      hover="true"
+      :hover="true"
     />
   </v-card>
 </template>
