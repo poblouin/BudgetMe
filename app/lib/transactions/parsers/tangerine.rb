6e36ce070cc36c4d20c2aa9672a151ba19d71560
poblouin <poblouin@protonmail.com>
2023-08-31 08:11:39 -0400

    nil check tangerine import

--- app/lib/transactions/parsers/tangerine.rb
+++ app/lib/transactions/parsers/tangerine.rb
@@ -6,8 +6,8 @@ module Transactions
     # Row format: Transaction date,Transaction,Name,Memo,Amount
     class Tangerine
       def parse(row:)
-        category = row[3].match(/Category: (.*)/)[1]
-        category = TransactionCategory::UNCATEGORIZED if category == 'Other'
+        category = row[3]&.match(/Category: (.*)/).to_a[1]
+        category = TransactionCategory::UNCATEGORIZED if !category || category == 'Other'
 
         {
           date: Date.strptime(row[0], '%m/%d/%Y'),
